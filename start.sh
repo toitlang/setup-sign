6cad8cac673226fb93923a561878bbd630cb827d
Florian Loitsch <florian@toit.io>
2025-02-26 14:04:44 +0100

    Improve start script.

--- start.sh
+++ start.sh
@@ -4,11 +4,14 @@
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 password="$(cat $script_dir/password.secret)"
-key=d7c78f453acfaa35791f0232f351465c6d16ab94
+certum_sh="$script_dir/certum.sh"
+pin_secret="$script_dir/pin.secret"
+pkcs11_so="$script_dir/manager/sc30pkcs11-3.0.6.68-MS.so"
 certificate="$script_dir/certificate.pem"
+key=d7c78f453acfaa35791f0232f351465c6d16ab94
 
 $script_dir/server \
     --verbosity-level=debug \
     --password="$password" \
     --port=9876 \
-    bash $script_dir/certum.sh pin.secret "$script_dir/manager/sc30pkcs11-3.0.6.68-MS.so" "$certificate" $key
+    bash "$certum_sh" "$pin_secret" "$pkcs11_so" "$certificate" "$key"
